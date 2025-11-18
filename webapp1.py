2fe9539e056094e3c605582231167a420fed27ca
sureshgoud.donta <57621536+Sureshgoudjsm@users.noreply.github.com>
2025-11-18 18:37:35 +0530

    Update webapp1.py

--- webapp1.py
+++ webapp1.py
@@ -141,8 +141,7 @@ st.markdown(
     </style>
     
     <div class="center-content">
-        
-        <h1 class="title-text">Testing </h1>
+        <h1 class="title-text">Testing</h1>
         <p class="subtitle-text">Analyze job details and your own skills simultaneously to generate a match score and tracking data.</p>
     </div>
     """, 
